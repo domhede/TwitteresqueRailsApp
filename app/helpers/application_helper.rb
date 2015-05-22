32488cce78e12b41137c7b1935a8c06492cff3e3
Dom Hede <domhede@gmail.com>
2015-05-22 05:07:59 +0000

    Filled in Layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Return the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Bandstand"
+    base_title = "Sample"
     if page_title.empty?
       base_title
     else
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,18 +5,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Bandstand"
+    assert_select "title", "Sample"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Bandstand"
+    assert_select "title", "Help | Sample"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Bandstand"
+    assert_select "title", "About | Sample"
+  end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Sample"
   end
 end
