fafe4c21333adeb2d1ceab360f42f6ab1ec86d49
Dom Hede <domhede@gmail.com>
2015-06-02 07:19:46 +0000

    Finish Log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get 'about'     =>  'static_pages#about'
   get 'contact'   =>  'static_pages#contact'
   get 'signup'    =>  'users#new'
-  get 'login'     =>  'session#new'
-  post 'logout'   =>  'sessions#create'
+  get 'login'     =>  'sessions#new'
+  post 'login'   =>  'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :users
 
