b3f07fcf83f8a25da21d5d90ba0e27fc57525bf3
Dom Hede <domhede@gmail.com>
2015-06-30 07:28:53 +0000

    Users Functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   
   def new
     @user = User.new
@@ -18,9 +24,41 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+   
+  end
+  
+  def update
+    
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else 
+       render 'edit'
+    end
+  end
+  
+  
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                   :password_confirmation)
+    end
+    # Before Filters
+    
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    #Confirms the correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                 password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
   end
   
   test "valid signup information" do
@@ -21,9 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                 password_confirmation: "password" }
     end
     assert_template 'users/show'
-    assert_not flash.FILL_IN
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    assert_select 'div.alert-success'
     assert is_logged_in?
 
   end
