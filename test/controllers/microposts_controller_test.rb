c8bf40b6174e429fc0705f128b09d1caab2d9084
Dom Hede <domhede@gmail.com>
2015-07-24 07:05:57 +0000

    Lesson 11 1.07

---
+++ test/controllers/microposts_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class MicropostsControllerTest < ActionController::TestCase
+  def setup
+    @micropost = microposts(:donut)
+  end
+  
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post :create, micropost: { content: "Lorm ipsum" }
+    end
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, id: @micropost
+    end
+    assert_redirected_to login_url
+  end  
+
+      
+end
