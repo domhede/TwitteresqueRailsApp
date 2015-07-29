d6b40ace816143764f40be9eadbe6d115ce2683c
Dom Hede <domhede@gmail.com>
2015-07-29 06:13:17 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'account_activations/edit'
-
   root              'static_pages#home'
   
   get 'help'      =>  'static_pages#help'
@@ -14,8 +8,16 @@ Rails.application.routes.draw do
   get 'login'     =>  'sessions#new'
   post 'login'   =>  'sessions#create'
   delete 'logout' => 'sessions#destroy'
+  resources :users do 
+    member do 
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]  
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+  
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -77,4 +77,33 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+  
+  test "should follow and unfollow a user" do
+    dom = users(:dom)
+    archer = users(:archer)
+    assert_not dom.following?(archer)
+    dom.follow(archer)
+    assert dom.following?(archer)
+    assert archer.followers.include?(dom)
+    dom.unfollow(archer)
+    assert_not dom.following?(archer)
+  end
+  
+  test "feed should have the right posts" do
+    dom = users(:dom)
+    archer = users(:archer)
+    lana = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert dom.feed.include?(post_following)
+    end
+    # Posts from self
+    dom.microposts.each do |post_self|
+      assert dom.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not dom.feed.include?(post_unfollowed)
+    end
+  end
 end
